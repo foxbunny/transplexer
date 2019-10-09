2a38cada82543ff2f3cf21f3e4e5cb6867eb3ff3
Hajime Yamasaki Vukelic <hayavuk@gmail.com>
2019-10-09 16:20:28 +0200

    Add array-like functions
    Added methods to the pipe objects that allows in-place manipulation of
    the transformer chain. Cleaned up the docs and removed inline
    documentation from the source code (moved to README).

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,23 @@
 import pipe from './index.js';
 
 describe('pipe', function () {
+  function add1(next) {
+    return function (x) {
+      next(x + 1);
+    };
+  }
+
+  function double(next) {
+    return function (x) {
+      next(x * 2);
+    }
+  }
+
+  function toString(next) {
+    return function (x) {
+      next('number = ' + x);
+    }
+  }
 
   test('relay a value from producer to consumer', function () {
     const consumer = jest.fn();
@@ -54,25 +71,13 @@ describe('pipe', function () {
   });
 
   test('multiple transformers', function () {
-    function t1(next) {
-      return function (x) {
-        next(x + 1);
-      };
-    }
-
-    function t2(next) {
-      return function (x) {
-        next(x * 2);
-      }
-    }
-
     const c1 = jest.fn();
-    const p1 = pipe(t1, t2);
+    const p1 = pipe(add1, double);
     p1.connect(c1);
     p1.send(1);
 
     const c2 = jest.fn();
-    const p2 = pipe(t2, t1);
+    const p2 = pipe(double, add1);
     p2.connect(c2);
     p2.send(1);
 
@@ -91,32 +96,112 @@ describe('pipe', function () {
   });
 
   test('extend pipe', function () {
-    function t1(next) {
-      return function (x) {
-        next(x + 1);
-      };
-    }
-
-    function t2(next) {
-      return function (x) {
-        next(x * 2);
-      }
-    }
-
-    function t3(next) {
-      return function (x) {
-        next('number = ' + x);
-      }
-    }
-
     const c = jest.fn();
-    const p = pipe(t1);
-    const p1 = p.extend(t2, t3);
+    const p = pipe(add1);
+    const p1 = p.extend(double, toString);
     p1.connect(c);
     p.send(1);
 
     expect(c).toHaveBeenCalledWith('number = 4');
   });
 
+  test('push transformers', function () {
+    const c = jest.fn();
+    const p = pipe(add1, double);
+    p.connect(c);
+    p.push(toString);
+    p.send(1);
+
+    expect(c).toHaveBeenCalledWith('number = 4');
+  });
+
+  test('push multiple transformers', function () {
+    const c = jest.fn();
+    const p = pipe(add1);
+    p.connect(c);
+    p.push(double, toString);
+    p.send(1);
+
+    expect(c).toHaveBeenCalledWith('number = 4');
+  });
+
+  test('pop a transformer', function () {
+    const c = jest.fn();
+    const p = pipe(add1, double, toString);
+
+    p.connect(c);
+    const popped = p.pop();
+    p.send(1);
+
+    expect(c).toHaveBeenCalledWith(4);
+    expect(popped).toBe(toString);
+  });
+
+  test('remove at index', function () {
+    const c = jest.fn();
+    const p = pipe(add1, add1, double, toString);
+
+    p.connect(c);
+    p.pop(2);
+    p.send(1);
+
+    expect(c).toHaveBeenCalledWith('number = 3');
+  });
+
+  test('shift a transformer', function () {
+    const c = jest.fn();
+    const p = pipe(add1, double, toString);
+
+    p.connect(c);
+    const shifted = p.shift();
+    p.send(1);
+
+    expect(c).toHaveBeenCalledWith('number = 2');
+    expect(shifted).toBe(add1);
+  });
+
+  test('unshift a transformer', function () {
+    const c = jest.fn();
+    const p = pipe(add1, double, toString);
+
+    p.connect(c);
+    p.unshift(add1);
+    p.send(1);
+
+    expect(c).toHaveBeenCalledWith('number = 6');
+  });
+
+  test('unshift multiple transformers', function () {
+    const c = jest.fn();
+    const p = pipe(toString);
+
+    p.connect(c)
+    p.unshift(add1, double);
+    p.send(1);
+
+    expect(c).toHaveBeenCalledWith('number = 4');
+  });
+
+  test('remove a specified transformer', function () {
+    const c = jest.fn();
+    const p = pipe(add1, double, toString);
+
+    p.connect(c);
+    p.remove(double);
+    p.send(1);
+
+    expect(c).toHaveBeenCalledWith('number = 2');
+  });
+
+  test('remove all copies of the transformer', function () {
+    const c = jest.fn();
+    const p = pipe(add1, add1, double, toString);
+
+    p.connect(c);
+    p.remove(add1);
+    p.send(1);
+
+    expect(c).toHaveBeenCalledWith('number = 2');
+  });
 });
 
